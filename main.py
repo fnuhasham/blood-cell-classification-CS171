6f1db9684787308ae49351aee1661047e3bfd10c
Fnu Hasham <fnu.hasham@sjsu.edu>
2026-04-15 21:03:52 -0700

    Add CNN baseline code

---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+
+DATASET_PATH = "."
+
+print("Folders found:")
+print(os.listdir(DATASET_PATH))
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+
+DATASET_PATH = "."
+
+IMG_SIZE = (128, 128)
+BATCH_SIZE = 32
+SEED = 42
+
+train_data = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="training",
+    seed=SEED,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+val_data = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="validation",
+    seed=SEED,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_data.class_names
+print("Classes:", class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_data = train_data.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_data = val_data.cache().prefetch(buffer_size=AUTOTUNE)
+
+model = models.Sequential([
+    layers.Rescaling(1./255, input_shape=(128, 128, 3)),
+
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(128, (3, 3), activation="relu"),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
+
+    layers.Flatten(),
+    layers.Dense(128, activation="relu"),
+    layers.Dropout(0.5),
+
+    layers.Dense(len(class_names), activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=10
+)
+
+plt.plot(history.history["accuracy"], label="Training Accuracy")
+plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.title("Training vs Validation Accuracy")
+plt.show()
+
+val_loss, val_accuracy = model.evaluate(val_data)
+print("Validation Accuracy:", val_accuracy)
+
+y_true = []
+y_pred = []
+
+for images, labels in val_data:
+    predictions = model.predict(images)
+    predicted_labels = np.argmax(predictions, axis=1)
+
+    y_true.extend(labels.numpy())
+    y_pred.extend(predicted_labels)
+
+print("\nClassification Report:")
+print(classification_report(y_true, y_pred, target_names=class_names))
+
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_true, y_pred))
+
+model.save("blood_cell_cnn_model.keras")
+print("Model saved as blood_cell_cnn_model.keras")
