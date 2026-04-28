82b3cd5618e5bf097d2a0b54634aee4073e3caf0
Alder Wang <alderwang06@gmail.com>
2026-04-27 21:45:39 -0700

    parameter tuning

--- main.py
+++ main.py
@@ -37,23 +37,38 @@ class_names = train_data.class_names
 print("Classes:", class_names)
 
 AUTOTUNE = tf.data.AUTOTUNE
-train_data = train_data.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_data = val_data.cache().prefetch(buffer_size=AUTOTUNE)
 
 # Data augmentation to help with overfitting and improve generalization
+# Applied only to train dataset
 data_augmentation = tf.keras.Sequential([
-    layers.RandomFlip("horizontal"),
-    layers.RandomRotation(0.08),
-    layers.RandomZoom(0.10),
+    layers.RandomFlip("horizontal_and_vertical"),
+    layers.RandomRotation(0.3),
+    layers.RandomZoom(0.3),
+    layers.RandomContrast(0.3),
+    layers.RandomBrightness(0.2),
+    layers.RandomTranslation(0.1, 0.1),
 ], name="data_augmentation")
 
-weight_decay = 5e-4
+train_data = train_data.cache().shuffle(5000).map(
+    lambda x, y: (data_augmentation(x, training=True), y),
+    num_parallel_calls=AUTOTUNE
+).prefetch(buffer_size=AUTOTUNE)
+
+val_data = val_data.cache().prefetch(buffer_size=AUTOTUNE)
+
+weight_decay = 1e-3
 
 # Model has convolutional layers with L2 regularization and batch normalization
 model = models.Sequential([
     layers.Input(shape=(128, 128, 3)),
-    data_augmentation,
     layers.Rescaling(1./255),
+
+    layers.Conv2D(
+        16, (3, 3), activation="relu",
+        kernel_regularizer=regularizers.l2(weight_decay)
+    ),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(),
 
     layers.Conv2D(
         32, (3, 3), activation="relu",
@@ -68,27 +83,20 @@ model = models.Sequential([
     ),
     layers.BatchNormalization(),
     layers.MaxPooling2D(),
-
-    layers.Conv2D(
-        128, (3, 3), activation="relu",
-        kernel_regularizer=regularizers.l2(weight_decay)
-    ),
-    layers.BatchNormalization(),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.3),
+    layers.Dropout(0.4),
 
     layers.GlobalAveragePooling2D(),
     layers.Dense(
-        128, activation="relu",
+        64, activation="relu",
         kernel_regularizer=regularizers.l2(weight_decay)
     ),
-    layers.Dropout(0.5),
+    layers.Dropout(0.6),
 
     layers.Dense(len(class_names), activation="softmax")
 ])
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
     loss="sparse_categorical_crossentropy",
     metrics=["accuracy"]
 )
@@ -98,7 +106,7 @@ model.summary()
 history = model.fit(
     train_data,
     validation_data=val_data,
-    epochs=10
+    epochs=50
 )
 
 plt.plot(history.history["accuracy"], label="Training Accuracy")
